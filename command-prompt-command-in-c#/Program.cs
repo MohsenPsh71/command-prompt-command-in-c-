08cf3fa26462f8d34f0deb871bef80ac673c7013
mohsen <71mohsenpsh@gmail.com>
2024-10-25 19:40:16 +0330

    Delete file from Dir and replace that after download from url every 30 sec

--- command-prompt-command-in-c#/Program.cs
+++ command-prompt-command-in-c#/Program.cs
@@ -1,1 +1,26 @@
 ﻿
+using System.Diagnostics;
+while (true)
+{
+
+    #region Run As Administrator
+    Process p = new Process();
+    p.StartInfo.FileName = "cmd.exe";
+    p.StartInfo.Arguments = "/k Del C:\\test\\x-ui.db";
+    //p.StartInfo.UseShellExecute = false;
+    //p.StartInfo.CreateNoWindow = true;
+    //p.StartInfo.RedirectStandardOutput = true;
+    p.StartInfo.Verb = "runas";
+    p.Start();
+    #endregion
+
+    //Dowload Wget.exe and paste to C:\Windows\System32
+    Process.Start("cmd.exe", "/k wget -P C:\\test http://10.10.10.10/x-ui.db");
+
+    //ProcessStartInfo info = new ProcessStartInfo("cmd.exe");
+    //info.Arguments = "/K cd C:\\test && curl -O http://12.12.12.12/x-ui.db";
+    //Process.Start(info);
+
+
+    System.Threading.Thread.Sleep(30000);
+}
